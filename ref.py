4e787ada315c9b23f4b9733d48cb38e97de18417
LightningGod <78635998+LightningGod7@users.noreply.github.com>
2024-02-18 10:24:53 +0800

    Create ref.py
    script to read ref file & replace $LHOST with tun0 ip

---
+++ ref.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import sys
+import netifaces as ni
+
+def get_tun0_ip():
+    try:
+        TUN0_IP = ni.ifaddresses('tun0')[ni.AF_INET][0]['addr']
+        return TUN0_IP
+    except ValueError:
+        return None
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+
+def search_keyword(lines, keyword, lines_after=0):
+    found = False
+    print(keyword)
+    for i, line in enumerate(lines):
+        if keyword in line:
+            found = True
+            print(line.strip())
+            for j in range(1, lines_after + 1):
+                if i + j < len(lines):
+                    print(lines[i + j].strip())
+            if lines_after!=0:
+                break
+    if not found:
+        print(f"Keyword '{keyword}' not found in the file.")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        # If no arguments provided, simply output the whole file
+        lines = read_file('/pt/reference/basics')
+        tun0_ip = get_tun0_ip()
+        if tun0_ip:
+            for line in lines:
+                print(line.replace('$LHOST', tun0_ip).strip())
+        else:
+            print("Please connect to VPN to get the tun0 IP address.")
+    else:
+        keyword = sys.argv[1]
+        lines_after = int(sys.argv[2]) if len(sys.argv) > 2 else 0
+        tun0_ip = get_tun0_ip()
+        if tun0_ip:
+            lines = read_file('/pt/reference/basics')
+            for line in lines:
+                line = line.replace('$LHOST', tun0_ip)
+            search_keyword(lines, keyword, lines_after)
+        else:
+            print("Please connect to VPN to get the tun0 IP address.")
